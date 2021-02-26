8f0929ff4c21b2ee1507e46c491d6eadf31fda42
root <root@localhost.localdomain>
2021-02-26 13:47:54 +0200

    lol

--- pro/urls.py
+++ pro/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobs/', include('job.urls'))
+    path('jobs/', include('job.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
