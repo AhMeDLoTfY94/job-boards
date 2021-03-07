82166e63a66f6400207a8849c6cd0c2e2d062255
lotfy <lotfy@localhost.localdomain>
2021-03-07 11:01:05 +0200

    update Signup

--- accounts/models.py
+++ accounts/models.py
@@ -11,7 +11,7 @@ class City(models.Model):
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    city=models.ForeignKey(City,on_delete=models.CASCADE)
+    #city=models.ForeignKey(City,on_delete=models.CASCADE)
     phone_number=models.CharField(max_length=35)
     image=models.ImageField(upload_to='profile/')
 
