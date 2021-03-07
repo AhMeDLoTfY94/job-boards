6f16708ab1181e62a28279d0e32d89518d2db679
lotfy <lotfy@localhost.localdomain>
2021-03-07 11:21:00 +0200

    update PROFILE

--- accounts/models.py
+++ accounts/models.py
@@ -3,15 +3,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class City(models.Model):
-    name=models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    #city=models.ForeignKey(City,on_delete=models.CASCADE)
+    city=models.CharField(max_length=55,blank=True, null=True)
     phone_number=models.CharField(max_length=35)
     image=models.ImageField(upload_to='profile/')
 
